a913975d3789fe0894a21f4983e6e9cac449a603
AbdujalilovHasan <abdujalilovhasan171@gmail.com>
2024-09-20 20:22:23 +0500

    new update

--- src/components/ServicesSwiper/ServicesSwiper.jsx
+++ src/components/ServicesSwiper/ServicesSwiper.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import style from './ServicesSwiper.module.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
 import avatar1 from '../../assets/images/customer-1.svg';
 import avatar2 from '../../assets/images/customer-2.svg';
 import avatar3 from '../../assets/images/customer-3.svg';
-import star from '../../assets/images/star.svg';
-
+import star from '../../assets/images/star.svg'
 export class ServicesSwiper extends Component {
     render() {
         return (
@@ -24,24 +25,26 @@ export class ServicesSwiper extends Component {
                             slidesPerView={3}
                             spaceBetween={20}
                             pagination={{
-                                clickable: true,
+                                el: `.${style['swiper-pagination']}`,
+                                clickable: true, 
                             }}
                             navigation={{
-                                clickable: true,
+                                nextEl: '#swiper-button-next',
+                                prevEl: '#swiper-button-prev',
                             }}
                             modules={[Pagination, Navigation]}
                             breakpoints={{
+                                1024: {
+                                    slidesPerView: 2,
+                                },
                                 768: {
                                     slidesPerView: 2,
-                                    slidesPerScroll: 2,
                                 },
                                 425: {
                                     slidesPerView: 1,
-                                    slidesPerScroll: 1,
                                 },
                                 320: {
                                     slidesPerView: 1,
-                                    slidesPerScroll: 1,
                                 },
                             }}
                             className="mySwiper"
@@ -72,8 +75,8 @@ export class ServicesSwiper extends Component {
                                         <div className={style['card-top-names']}>
                                             <div><img src={avatar2} alt="avatar" /></div>
                                             <div className={style['card-top-name']}>
-                                                <h2>Viezh Robert</h2>
-                                                <p>Warsaw, Poland</p>
+                                                <h2>Yessica Christy</h2>
+                                                <p>Shanxi, China</p>
                                             </div>
                                         </div>
                                         <div className={style['card-top-star']}>
@@ -92,8 +95,8 @@ export class ServicesSwiper extends Component {
                                         <div className={style['card-top-names']}>
                                             <div><img src={avatar3} alt="avatar" /></div>
                                             <div className={style['card-top-name']}>
-                                                <h2>Viezh Robert</h2>
-                                                <p>Warsaw, Poland</p>
+                                                <h2>Kim Young Jou</h2>
+                                                <p>Seoul, South Korea</p>
                                             </div>
                                         </div>
                                         <div className={style['card-top-star']}>
@@ -132,8 +135,8 @@ export class ServicesSwiper extends Component {
                                         <div className={style['card-top-names']}>
                                             <div><img src={avatar2} alt="avatar" /></div>
                                             <div className={style['card-top-name']}>
-                                                <h2>Viezh Robert</h2>
-                                                <p>Warsaw, Poland</p>
+                                                <h2>Yessica Christy</h2>
+                                                <p>Shanxi, China</p>
                                             </div>
                                         </div>
                                         <div className={style['card-top-star']}>
@@ -152,6 +155,26 @@ export class ServicesSwiper extends Component {
                                         <div className={style['card-top-names']}>
                                             <div><img src={avatar3} alt="avatar" /></div>
                                             <div className={style['card-top-name']}>
+                                                <h2>Kim Young Jou</h2>
+                                                <p>Seoul, South Korea</p>
+                                            </div>
+                                        </div>
+                                        <div className={style['card-top-star']}>
+                                            <span>4.5</span>
+                                            <img src={star} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className={style['card-subtitle']}>
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className={style['slider-card']}>
+                                    <div className={style['card-top']}>
+                                        <div className={style['card-top-names']}>
+                                            <div><img src={avatar1} alt="avatar" /></div>
+                                            <div className={style['card-top-name']}>
                                                 <h2>Viezh Robert</h2>
                                                 <p>Warsaw, Poland</p>
                                             </div>
@@ -166,7 +189,54 @@ export class ServicesSwiper extends Component {
                                     </div>
                                 </div>
                             </SwiperSlide>
+                            <SwiperSlide>
+                                <div className={style['slider-card']}>
+                                    <div className={style['card-top']}>
+                                        <div className={style['card-top-names']}>
+                                            <div><img src={avatar2} alt="avatar" /></div>
+                                            <div className={style['card-top-name']}>
+                                                <h2>Yessica Christy</h2>
+                                                <p>Shanxi, China</p>
+                                            </div>
+                                        </div>
+                                        <div className={style['card-top-star']}>
+                                            <span>4.5</span>
+                                            <img src={star} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className={style['card-subtitle']}>
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className={style['slider-card']}>
+                                    <div className={style['card-top']}>
+                                        <div className={style['card-top-names']}>
+                                            <div><img src={avatar3} alt="avatar" /></div>
+                                            <div className={style['card-top-name']}>
+                                                <h2>Kim Young Jou</h2>
+                                                <p>Seoul, South Korea</p>
+                                            </div>
+                                        </div>
+                                        <div className={style['card-top-star']}>
+                                            <span>4.5</span>
+                                            <img src={star} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className={style['card-subtitle']}>
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    </div>
+                                </div>
+                            </SwiperSlide>
                         </Swiper>
+                        <div className={`${style["swiper-pagination"]}`}>
+                            <div id='swiper-pagination'></div>
+                        </div>
+                        <div className={`${style['swiper-navigation']}`}>
+                            <div id="swiper-button-prev" className={`${style["swiper-button-prev"]}`}><FaArrowLeft /></div>
+                            <div id="swiper-button-next" className={`${style["swiper-button-next"]}`}><FaArrowRight /></div>
+                        </div>
                     </div>
                 </div>
             </div>
